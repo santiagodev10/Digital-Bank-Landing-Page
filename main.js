6cd54a3c3d062a252a3e5ef49fe445c9e0132ee0
santiagodev <sanxp18@gmail.com>
2025-06-02 13:08:08 -0400

    add backdrop overlay that opens along with the dropdown menu to block interaction with all elements except header

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const hamMenu = document.querySelector('.ham-menu');
 const menu = document.querySelector('.nav-header--nav-links');
+const backdrop = document.querySelector('.nav-header--backdrop');
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('change');
     menu.classList.toggle('inactive');
     menu.classList.toggle('nav-links_show');
+    backdrop.classList.toggle('remove');
+    backdrop.classList.toggle('backdrop_overlay');
+});
+
+// Close menu when clicking outside
+backdrop.addEventListener('click', () => {
+    hamMenu.classList.remove('change');
+    menu.classList.add('inactive');
+    menu.classList.remove('nav-links_show');
+    backdrop.classList.add('remove');
+    backdrop.classList.remove('backdrop_overlay');
 });
