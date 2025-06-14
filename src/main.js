58bdeb7479405f0e90b06a0962bc84caa094942f
santiagodev <sanxp18@gmail.com>
2025-06-14 19:24:42 -0400

    add hover functionality for the footer svg icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ const backdrop = document.querySelector('.nav-header--backdrop');
 const header = document.querySelector('header');
 const main = document.querySelector('main');
 const footer = document.querySelector('footer');
+const svgIcons = document.querySelectorAll('.svg-icon');
+const hoverColor = 'var(--Lime-Green)';
+const defaultColor = '#FFF';
+let scrollToTopHandler; 
+
 
 function toggleMenu() {
     hamMenu.classList.toggle('change');
@@ -26,29 +31,21 @@ function handleKeyboardAccess() {
     }
 }
 
-hamMenu.addEventListener('click', toggleMenu);
+function handleSvgHover() {
+    svgIcons.forEach(icon => {
+        icon.addEventListener('mouseover', () => {
+            const svgIconPath = icon.querySelector('.svg-icon-path');
+            svgIconPath.setAttribute('fill', hoverColor);
+        });
 
-hamMenu.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        toggleMenu();
-    }
-});
-
-// Close menu when clicking outside
-backdrop.addEventListener('click', () => {
-    hamMenu.classList.remove('change');
-    menu.classList.add('inactive');
-    menu.classList.remove('nav-links_show');
-    backdrop.classList.add('remove');
-    backdrop.classList.remove('backdrop_overlay');
-    backdrop.setAttribute('aria-hidden', 'true');
-    main.inert = false;
-    footer.inert = false;
-});
+        icon.addEventListener('mouseout', () => {
+            const svgIconPath = icon.querySelector('.svg-icon-path');
+            svgIconPath.setAttribute('fill', defaultColor);
+        });
+    });
+}
 
 // Scroll to top functionality
-let scrollToTopHandler; 
-
 const scrollToTop = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -86,4 +83,25 @@ const scrollToTop = new IntersectionObserver((entries) => {
     });
 });
 
+hamMenu.addEventListener('click', toggleMenu);
+
+hamMenu.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        toggleMenu();
+    }
+});
+
+// Close menu when clicking outside
+backdrop.addEventListener('click', () => {
+    hamMenu.classList.remove('change');
+    menu.classList.add('inactive');
+    menu.classList.remove('nav-links_show');
+    backdrop.classList.add('remove');
+    backdrop.classList.remove('backdrop_overlay');
+    backdrop.setAttribute('aria-hidden', 'true');
+    main.inert = false;
+    footer.inert = false;
+});
+
+handleSvgHover();
 scrollToTop.observe(header);
